1938ab8af3e5d7d3717b3613840e03fb8be0e425
benjaminpil91@gmail.com <benjaminpil91@gmail.com>
2019-11-26 19:56:02 +0100

    Database connectie inloggen

--- DivisioLogin/MainWindow.xaml.cs
+++ DivisioLogin/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,18 +31,61 @@ namespace DivisioLogin
                 DragMove();
         }
 
-        private void OpenLogin(object sender, RoutedEventArgs e)
+       /* private void OpenLogin(object sender, RoutedEventArgs e)
         {
             Login_Window login = new Login_Window();
             this.Visibility = Visibility.Hidden;
             login.Show();
-        }
+        }*/
 
         private void OpenRegister(object sender, RoutedEventArgs e)
         {
             Register_Window register = new Register_Window();
             this.Visibility = Visibility.Hidden;
             register.Show();
+        }
+
+        public static string username;
+        public static string getusername
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=MSI;Initial Catalog=Divsio;Integrated Security=True;");
+            
+            try
+            {
+                if (sqlCon.State == System.Data.ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                getusername = TxtUsername.Text;    
+                String query = "SELECT COUNT(1) FROM tblUser WHERE Username=@Username AND Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query,sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", TxtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", TxtPassword.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    Login_Window login = new Login_Window();
+                    this.Visibility = Visibility.Hidden;
+                    login.Show();
+                }
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Username or password is incorrect.");
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
         }
     }
 }
